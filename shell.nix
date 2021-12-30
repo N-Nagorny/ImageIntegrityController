f44c8dbb98f53b620fa036c548e975eeef72f231
N-Nagorny <nikiton-n@mail.ru>
2021-12-30 22:57:23 +0300

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{}:
+
+with import <nixpkgs> {};
+with libsForQt512;
+
+stdenv.mkDerivation {
+  name = "ImageIntegrityController";
+  buildInputs = [ opencv3 qtbase ];
+  nativeBuildInputs = [ gnumake pkg-config qmake valgrind wrapQtAppsHook ];
+  src = ./.;
+  shellHook = ''
+    setQtEnvironment=$(mktemp)
+    random=$(openssl rand -base64 20 | sed "s/[^a-zA-Z0-9]//g")
+    makeWrapper "$(type -p sh)" "$setQtEnvironment" "''${qtWrapperArgs[@]}" --argv0 "$random"
+    sed "/$random/d" -i "$setQtEnvironment"
+    source "$setQtEnvironment"
+  '';
+}
